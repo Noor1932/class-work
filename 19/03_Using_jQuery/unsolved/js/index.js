6098e640f05fbb44e42233b7d536fa93ef777660
Noor1932 <78519651+Noor1932@users.noreply.github.com>
2023-04-18 12:07:55 -0500

    Js beginning

---
+++ 19/03_Using_jQuery/unsolved/js/index.js
@@ -0,0 +1,12 @@
+// Use jQuery to target the <h1> tag and make it red.
+$("h1"). css('color', 'red')
+// Use jQuery to target the blue class and make it blue.
+$(".blue"). css('color', 'blue')
+// Use jQuery to target the addMe ID and change its HTML to Hello.
+$('#addMe'). html('Hello')
+
+$('#clickme') .html('here')
+$('#clickme'). css('background-color', 'red')
+$('#clickme'). css('color', 'white')
+$('#clickme'). css('border-radius', '5px')
+
